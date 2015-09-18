b06999a00f42cd154c05f24bd370fe22591e6276
iosdevzone <idz@iosdeveloperzone.com>
2015-09-17 17:49:46 -0700

    [CocoaPods] Remove Pods dir and Podfile.lock to force latest ver.

--- Pods/Target Support Files/Pods-IDZSwiftCommonCryptoCocoaPodsTest/Pods-IDZSwiftCommonCryptoCocoaPodsTest-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IDZSwiftCommonCryptoCocoaPodsTest : NSObject
-@end
-@implementation PodsDummy_Pods_IDZSwiftCommonCryptoCocoaPodsTest
-@end
